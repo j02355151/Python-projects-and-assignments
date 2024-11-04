2fbaf4b3de8d21d0712c838bc59ad814ee1d27c7
Chun Lau <158354108+j02355151@users.noreply.github.com>
2024-11-04 13:12:46 -0500

    Add files via upload

---
+++ Record_name_and_age/main.py
@@ -0,0 +1,8 @@
+
+Name = input('What is your name?')
+Age = input('How old are you?')
+year_born = 2021 - int(Age)
+
+print('Hello', Name + '!', 'You were born in', year_born)
+
+input('Press ENTER to exit')
